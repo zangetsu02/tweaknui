f51475d72dedb1c93451f7329f1c3c25313380cb
zangetsu002 <christian.palladini2@gmail.com>
2025-07-02 13:39:07 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+    '@nuxt/fonts'
+  ]
+})
