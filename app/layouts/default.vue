0af3c896acc8aa353197239ff9d464ca188adf85
zangetsu002 <christian.palladini2@gmail.com>
2025-07-02 21:43:34 +0200

    feat: default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <UApp :tooltip="{ delayDuration: 0 }" class="relative">
+    <div class="bg-background text-foreground flex min-h-[100dvh] flex-col items-center justify-items-center">
+      <AppHeader />
+      <main class="w-full flex-1">
+        <slot />
+      </main>
+    </div>
+  </UApp>
+</template>
